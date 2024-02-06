f61acff8a04c5256dfbcb4c3323b0de4df091051
Adam Němeček <nemeceka22@spstrutnov.cz>
2024-02-06 11:19:13 +0100

    začátek koblihy
    Vlkovi jsem utekl

--- Kobliha/Koblizek.cs
+++ Kobliha/Koblizek.cs
@@ -1,9 +1,27 @@
-﻿using System;
-namespace Kobliha { }
-
-public class Class1
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace Kobliha
 {
-	public Class1()
+	public class Koblizek
 	{
+        private int PoziceX { get; set; }
+        private int PoziceY { get; set; }
+        private Keys OvladaniDoleva { get; set; }
+        private Keys OvladaniDoprava { get; set; }
+
+
+
+        public Koblizek(Keys smerDoleva, Keys smerDoprava, int X = 1000, int Y = 500)
+		{
+			PoziceX = X;
+			PoziceY = Y;
+
+            OvladaniDoleva = smerDoleva;
+            OvladaniDoprava = smerDoprava;
+
+
+        }
+
 	}
 }
