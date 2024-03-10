3f0217a62b8b118c46907c0fcd53c355d2ba3952
Tomáš Celba <celbat22@spstrutnov.com>
2024-03-10 12:56:49 +0100

    Nová NPC classa
    koblha

---
+++ Kobliha/NPC.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kobliha
+{
+    internal class NPC
+    {
+        protected int Velikost { get; set; }
+        private int PoziceX { get; set; }
+        private int PoziceY { get; set; }
+
+        public NPC(GraphicsDevice grafickeZarizeni, int velikost, string nazevSouboru)
+        {
+
+        }
+        public void VykresliSe(SpriteBatch vykreslovaciDavka)
+        {
+
+        }
+    }
+}
