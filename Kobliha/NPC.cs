bd6f4ccda82e0e02d6936220e46fc48f0fa3a660
Adam Němeček <nemeceka22@spstrutnov.cz>
2024-03-10 17:02:15 +0100

    dedek hotov

--- Kobliha/NPC.cs
+++ Kobliha/NPC.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using SharpDX.Direct3D9;
+using SharpDX.XAudio2;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,17 +14,16 @@ namespace Kobliha
     public class NPC
     {
         protected int Velikost { get; set; }
-        private int PoziceX { get; set; }
-        private int PoziceY { get; set; }
+        private Vector2 Pozice { get; set; }
         protected Texture2D Textura { get; set; }
 
         public NPC(GraphicsDevice grafickeZarizeni, int velikost, string nazevSouboru,
             int rozmerOknaX = 0, int rozmerOknaY = 0)
         {
             Velikost = velikost;
+            Pozice = new Vector2(rozmerOknaX / 2, rozmerOknaY - velikost);
 
-            PoziceX = rozmerOknaX / 2;
-            PoziceY = rozmerOknaY - velikost;
+
 
             using (FileStream stream = new FileStream(nazevSouboru, FileMode.Open))
             {
@@ -31,7 +32,7 @@ namespace Kobliha
         }
         public void VykresliSe(SpriteBatch vykreslovaciDavka)
         {
-            vykreslovaciDavka.Draw(Textura, new Vector2(PoziceX + Velikost / 2, PoziceY + Velikost / 2));
+            vykreslovaciDavka.Draw(Textura, Pozice, Color.White);
         }
     }
 }
