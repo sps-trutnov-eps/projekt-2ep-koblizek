3fba9431937410151636cd4b2aab0cea6b487965
Adam Němeček <nemeceka22@spstrutnov.cz>
2024-03-17 12:34:50 +0100

    vytvoření třídy na další část hry

---
+++ Kobliha/SecondScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Kobliha
+{
+    public class SecondScreen
+    {
+        public SecondScreen(/* případné parametry */)
+        {
+            // Inicializace druhé obrazovky
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Kreslení druhé obrazovky
+        }
+    }
+}
